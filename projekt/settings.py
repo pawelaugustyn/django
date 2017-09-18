2f425a7207eb60614fb45d06fb2a12780bef3198
Paweł Augustyn <pawelaugustyn15@gmail.com>
2017-09-18 21:23:10 +0200

    Switched off debug mode

--- projekt/settings.py
+++ projekt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v6#)xy*xfz70pqa=!wj9i%8r9bcwth*+&mnmc&r9lpusx*cpi%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
